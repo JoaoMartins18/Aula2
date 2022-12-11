98760de92bd5c47d78597d82b63742ef722416bd
JoaoMartins18 <120115422+JoaoMartins18@users.noreply.github.com>
2022-12-11 13:52:55 +0000

    Atualização

---
+++ main.py
@@ -0,0 +1,13 @@
+x1 = float(input("Qual a coordenada X do ponto 1? "))
+y1 = float(input("Qual a coordenada Y do ponto 1? "))
+
+x2 = float(input("Qual a coordenada X do ponto 2? "))
+y2 = float(input("Qual a coordenada Y do ponto 2? "))
+
+
+
+
+
+tamanho = (   (x2-x1)**2 + (y2-y1)**2   )    **  (1/2)
+
+print(f"O módulo do vetor é igual a {tamanho}")
